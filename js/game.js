a8842e2bdcf5e5af8b31018ba9f40c4c216fd674
Tommy Svendsen <tommysven@gmail.com>
2013-02-15 08:46:50 +0100

    small adjustments

--- js/game.js
+++ js/game.js
@@ -1,21 +1,21 @@
 (function (env) {
     env.addEventListener("load", function () {
-    	var score = 0;
-        var Q = Quintus({ audioSupported: [ 'mp3','wav' ] }).
+        var score = 0;
+        var Q = Quintus({ audioSupported: [ 'mp3', 'wav' ] }).
             include('Audio, Sprites, Anim, Scenes, Input, 2D, Touch, UI').
             setup({maximize: true}).
             controls(true).
             enableSound();
 
-        var interval = setInterval(function() {
-            score = score+1;
+        var interval = setInterval(function () {
+            score = score + 1;
             Q.stageScene('hud', 1);
-            if(score === 120) {
-                Q.stageScene('endGame',1);
+            if (score === 120) {
+                Q.stageScene('endGame', 1);
                 Q.stage(0).pause();
                 clearInterval(interval);
             }
-        },1000);
+        }, 1000);
 
         Q.input.joypadControls();
 
@@ -56,110 +56,130 @@
             },
             step: function (dt) {
                 if (this.p.stepping) {
-                    if(this.p.origX < this.p.destX){
+                    if (this.p.origX < this.p.destX) {
                         this.play("run_right");
                     }
-                    else if(this.p.origX > this.p.destX){
+                    else if (this.p.origX > this.p.destX) {
                         this.play("run_left");
                     }
-                    if(this.p.origY > this.p.destY){
+                    if (this.p.origY > this.p.destY) {
                         this.play("run_up");
-                    } else if(this.p.origY < this.p.destY){
+                    } else if (this.p.origY < this.p.destY) {
                         this.play("run_down");
                     }
-                } else{
+                } else {
                     this.play("standing");
 
                 }
 
             }
         });
-        
+
         Q.Sprite.extend('Question', {
-            init: function(p) {
+            init: function (p) {
                 this._super(p, {
                     sprite: 'Question',
                     sheet: 'Question',
                     gravity: 0
                 });
                 this.add('2d');
-                this.on('hit.sprite', function(collision) {
+                this.on('hit.sprite', function (collision) {
                     if (collision.obj.isA('OlaNordmann')) {
-	                    this.destroy();	
-                    	Q.stageScene('showQuestion', 1, {
-                    		questionText: "Hva heter onkelen til Donald Duck?", 
-                    		alternativeA: "Onkel Skrue", 
-                    		alternativeB: "Fetter Anton", 
-                    		correctAnswer: "A"
-                    	});
+
+                        Q.stageScene('showQuestion', 1, {
+                            questionText: "Hva heter onkelen til Donald Duck?",
+                            alternativeA: "Onkel Skrue",
+                            alternativeB: "Fetter Anton",
+                            correctAnswer: "A",
+                            q: this
+                        });
                     }
                 });
             }
         });
-        
-        
-		Q.scene('showQuestion', function(stage) {
-			var container = stage.insert(new Q.UI.Container({
-				x : Q.width / 2,
-				y : Q.height / 2,
-				fill : "rgba(0,0,0,0.6)"
-			}));
-			
-			var question = container.insert(new Q.UI.Text({
-				x : 0,
-				y : 0,
-				color: "white",
-				size: 16,
-				label : stage.options.questionText
-			}));
-			
-			var buttonA = container.insert(new Q.UI.Button({
-				x : 0,
-				y : question.p.h + 20,
-				fontColor: "white",
-				font: "800 16px arial",
-				fill : "#CCCCCC",
-				label : stage.options.alternativeA
-			}));
-			
-			var buttonB = container.insert(new Q.UI.Button({
-				x : 0,
-				y : question.p.h + 10 + buttonA.p.h + 20,
-				fontColor: "white",
-				font: "800 16px arial",
-				fill : "#CCCCCC",
-				label : stage.options.alternativeB
-			}));
-			
-			buttonA.on("click", function() {
-				console.log("buttonA clicked, correct answer", stage.options.correctAnswer);
-			});
-			
-			buttonB.on("click", function() {
-				console.log("buttonB clicked, correct answer", stage.options.correctAnswer);
-			});
 
-			container.fit(20);
-		});        
+
+        Q.scene('showQuestion', function (stage) {
+            var container = stage.insert(new Q.UI.Container({
+                x: Q.width / 2,
+                y: Q.height / 2,
+                fill: "rgba(0,0,0,0.6)"
+            }));
+
+            var question = container.insert(new Q.UI.Text({
+                x: 0,
+                y: 0,
+                color: "white",
+                size: 16,
+                label: stage.options.questionText
+            }));
+
+            var buttonA = container.insert(new Q.UI.Button({
+                x: 0,
+                y: question.p.h + 20,
+                fontColor: "white",
+                font: "800 16px arial",
+                fill: "#CCCCCC",
+                label: stage.options.alternativeA
+            }));
+
+            var buttonB = container.insert(new Q.UI.Button({
+                x: 0,
+                y: question.p.h + 10 + buttonA.p.h + 20,
+                fontColor: "white",
+                font: "800 16px arial",
+                fill: "#CCCCCC",
+                label: stage.options.alternativeB
+            }));
+
+            buttonA.on("click", function () {
+                console.log("buttonA clicked, correct answer", stage.options.correctAnswer);
+                stage.options.q.destroy();
+            });
+
+            buttonB.on("click", function () {
+                console.log("buttonB clicked, correct answer", stage.options.correctAnswer);
+                stage.options.q.destroy();
+            });
+
+            container.fit(20);
+        });
+
+        var findEmptyTiles = function (tiles) {
+            var found = [];
+            for (var i = 0; i < tiles.length; i++) {
+                for (var j = 0; j < tiles[i].length; j++) {
+                    if (!tiles[i][j]) {
+                        found.push({y: (i * 16), x: (j * 16)})
+                    }
+                }
+            }
+            return found;
+        }
+
 
         Q.scene('level', function (stage) {
-            stage.collisionLayer(new Q.TileLayer({
+            var tileLayer = new Q.TileLayer({
                 dataAsset: 'level.json',
                 sheet: 'Tiles',
                 tileW: 16,
                 tileH: 16
-            }));
-            stage.insert(new Q.Question({x: 248, y:380 }));
-            stage.insert(new Q.Question({x: 198, y:190 }));
-            stage.insert(new Q.Question({x: 248, y:470 }));
+            });
+            var freeAreas = findEmptyTiles(tileLayer.p.tiles);
+            stage.collisionLayer(tileLayer);
+            var numQs = Math.floor((Math.random() * 10) + 1);
+            for (var q = 0; q < numQs; q++) {
+                var pos = freeAreas[Math.floor((Math.random() * freeAreas.length - 1) + 1)];
+                stage.insert(new Q.Question(pos));
+            }
             var hero = stage.insert(new Q.OlaNordmann({ x: 0, y: 820 }));
             stage.add('viewport').follow(hero);
         });
 
-        Q.scene('hud', function(stage) {
+        Q.scene('hud', function (stage) {
             var pensionLbl = stage.insert(new Q.UI.Text({
                 x: Q.width / 2,
-                y:20,
+                y: 20,
                 align: 'center',
                 family: 'Monospace',
                 size: 16,
@@ -167,16 +187,16 @@
             }));
         });
 
-        Q.scene('endGame', function(stage) {
+        Q.scene('endGame', function (stage) {
             stage.insert(
                 new Q.UI.Text({
-                    x:Q.width/2,
-                    y:Q.height/2,
+                    x: Q.width / 2,
+                    y: Q.height / 2,
                     family: 'Monospace',
                     size: 24,
                     label: 'Dun dun dun...Game over!'
                 }))
-                Q.play('game_over.mp3');
+            Q.play('game_over.mp3');
         });
 
         Q.load('main_theme.mp3, game_over.mp3, sprites.png, sprites.json, level.json', function () {
@@ -187,6 +207,43 @@
             Q.stageScene('hud', 1)
         });
 
+        var questions = [
+            {
+                questionText: "Hva heter onkelen til Donald Duck?",
+                alternativeA: "Onkel Skrue",
+                alternativeB: "Fetter Anton",
+                correctAnswer: "A"
+            },
+            {
+                questionText: "Hva heter onkelen til Donald Duck?",
+                alternativeA: "Onkel Skrue",
+                alternativeB: "Fetter Anton",
+                correctAnswer: "A"
+            },
+            {
+                questionText: "Hva heter onkelen til Donald Duck?",
+                alternativeA: "Onkel Skrue",
+                alternativeB: "Fetter Anton",
+                correctAnswer: "A"
+            },
+            {
+                questionText: "Hva heter onkelen til Donald Duck?",
+                alternativeA: "Onkel Skrue",
+                alternativeB: "Fetter Anton",
+                correctAnswer: "A"
+            },
+            {
+                questionText: "Hva heter onkelen til Donald Duck?",
+                alternativeA: "Onkel Skrue",
+                alternativeB: "Fetter Anton",
+                correctAnswer: "A"
+            }
+
+        ];
+
 
     });
-}(window));
+}
+    (window)
+    )
+;
